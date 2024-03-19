e9d374053d545056cb627cd8ad162981b09b332d
cneicy <im@crash.work>
2024-03-20 00:09:12 +0800

    level4

--- Assets/Script/Game/Object/EndPoint.cs
+++ Assets/Script/Game/Object/EndPoint.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using Script.Init;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 namespace Script.Game.Object
@@ -12,19 +13,51 @@ namespace Script.Game.Object
         private const int TargetAlpha = 255;
         private bool _loadTrigger;
         private AudioSource _audioSource;
+        private GameObject _player;
         [SerializeField] private Loader.Scene scene;
+        private bool isEnded;
+        private Rigidbody2D _rigidbody2D;
+        private Light2D _light2D;
+
         private void Start()
         {
+            _player = GameObject.FindWithTag("Player");
+            _light2D = _player.GetComponentInChildren<Light2D>();
+            _rigidbody2D = _player.GetComponent<Rigidbody2D>();
+            
             _audioSource = GetComponent<AudioSource>();
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            _loadTrigger = true;
             _audioSource.Play();
-            StartCoroutine(Next());
+            if (Init.Init.Scene != Loader.Scene.Level4)
+            {
+                _loadTrigger = true;
+                StartCoroutine(Next());
+            }
+            else
+            {
+                _player.GetComponent<Rigidbody2D>().gravityScale = -1;
+                spriteRenderer.color = Color.white;
+                isEnded = true;
+            }
         }
 
+        private void EndGame()
+        {
+            
+            var temp = _rigidbody2D.velocity;
+            temp.x = Mathf.Clamp(temp.x, 0, 0);
+            temp.y = 2;
+            _rigidbody2D.velocity = temp;
+            _light2D.intensity+=0.001f;
+            _light2D.pointLightOuterRadius+=0.1f;
+            /*_light2D.intensity = 0;
+            var color = spriteRenderer.color;
+            color.a = Mathf.Lerp(color.a,TargetAlpha, 0.01f * Time.deltaTime);
+            spriteRenderer.color = color;*/
+        }
         private IEnumerator Next()
         {
             yield return new WaitForSecondsRealtime(1);
@@ -33,10 +66,10 @@ namespace Script.Game.Object
         }
         private void FixedUpdate()
         {
+            if(isEnded) EndGame();
             if (!_loadTrigger || TargetAlpha == (int)spriteRenderer.color.a) return;
             var color = spriteRenderer.color;
             color.a = Mathf.Lerp(color.a,TargetAlpha, 0.01f * Time.deltaTime);
-            
             spriteRenderer.color = color;
         }
     }
---
+++ Assets/Script/Game/UI/Notice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Script.Game.UI
+{
+    public class Notice : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text tmp;
+        private KeySettingManager _keySettingManager;
+        private void Awake()
+        {
+            _keySettingManager = GameObject.FindWithTag("KeySettingManager").GetComponent<KeySettingManager>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            switch (gameObject.tag)
+            {
+                case "NoticeWall":
+                    tmp.text = "墙跳: " + _keySettingManager.GetKey("Jump");
+                    break;
+                case "NoticeJump":
+                    tmp.text = "跳跃: " + _keySettingManager.GetKey("Jump");
+                    break;
+                case "NoticeDash":
+                    tmp.text = "冲刺: " + _keySettingManager.GetKey("Dash");
+                    break;
+            }
+            
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.tag.Equals("Player"))
+            {
+                tmp.text = "";
+            }
+        }
+    }
+}
