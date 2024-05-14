c8b51999f03d07bd82f98e6f93b0e1969eac297a
Eicy <im@crash.work>
2024-05-14 21:05:17 +0800

    multiplay

---
+++ Assets/Script/Server/ClientNetworkTransform.cs
@@ -0,0 +1,14 @@
+using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace Script.Server
+{
+    [DisallowMultipleComponent]
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+}
